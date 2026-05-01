0cb3c5bc176dc54a8f2a17fce0fa9d0059c425d8
McGeorge24 <steiner.jure@gmail.com>
2026-05-01 21:35:55 +0200

    renamed main to main_debug

--- main_debug.cpp
+++ main_debug.cpp
@@ -223,7 +223,7 @@ int main()
     int height = 100 + (2 * n - 1) * A * 3 + A;
     // int r = 25 + (2 * n - 1) * B;
 
-    std::ofstream maze_svg("maze.svg", std::fstream::trunc);
+    std::ofstream maze_svg("example_maze.svg", std::fstream::trunc);
     // specifikacije SVG slike
     maze_svg << "<svg version=\"1.1\" width=\"" << width << "\" height=\"" << height << "\" xmlns=\"http://www.w3.org/2000/svg\">";
     // ozadje
